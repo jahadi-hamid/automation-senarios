f5547024751c39df4d1eda6627477b31e95ed1f5
h.jahadi <jahadi.hamid@gmail.com>
2023-01-31 20:26:39 +0330

    fix install pkgs

--- terraform-ansible-jenkins-cicd/terraform/build/variable.tf
+++ terraform-ansible-jenkins-cicd/terraform/build/variable.tf
@@ -23,5 +23,5 @@ variable "ip_range" {
 }
 variable "home_project" {
   type = string
-  default = "/home/h.jahadi/MyGitRepo/automation-senarios/terraform-ansible-jenkins-cicd"
+  default = "/home/hamidj/MyGitRepo/automation-senarios/terraform-ansible-jenkins-cicd"
 }
